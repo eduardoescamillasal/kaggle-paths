b502ce862ec2e487f41e7bf68f65260f9d43675c
Daniel Reuter <dreuter@kth.se>
2019-09-17 07:31:19 +0200

    upd

--- household-power/household_power_consumption.py
+++ household-power/household_power_consumption.py
@@ -1,5 +1,31 @@
 #%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
 import powerdata
-powerdata.fetch_power_data()
-powerdataframe = powerdata.load_power_data()
+sns.set_style(style='whitegrid')
+
+metadata = powerdata.metadata_list()
+powerdataframe = powerdata.load_and_preprocess_data()
+#%%
+powerdataframe.head()
+
+#%%
+powerdataframe.info()
+
+#%%
+powerdataframe.describe()
+
+#%%
+startday = 27
+timefrom = '2006-12-' + str(startday) + ' 00:00'
+timeto = '2006-12-' + str(startday+1) + ' 00:00'
+powerdataframe[["S1"]][timefrom:timeto].plot(figsize = (15,8))
+plt.legend(fontsize=15)
+plt.show()
+
+#%%
+metadata[2]["S3"]
+
 #%%
